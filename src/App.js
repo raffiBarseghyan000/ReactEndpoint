abe6024d701c732ce8a32e04c0c62702ed781cfd
Raffi Barseghyan <raffi.barseghyan@fluxtechs.com>
2019-05-08 18:13:38 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {BrowserRouter, Route, Redirect, Link, Switch} from 'react-router-dom'
+import Main from './main.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false
+        }
+        this.handleUserChange = this.handleUserChange.bind(this)
+        this.handlePassChange = this.handlePassChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    async loginCall(username, password) {
+        const result = await fetch("http://localhost:9999/login", {
+            method: 'POST',
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        const resultMessage = await result.text()
+        return resultMessage
+    }
+
+    handleUserChange(event) {
+        this.setState({username: event.target.value})
+    }
+
+    handlePassChange(event) {
+        this.setState({password: event.target.value})
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        return this.loginCall(this.state.username, this.state.password).then((result)=> {
+            // if (JSON.parse(result).success === false) {
+            //     alert(JSON.parse(result).message)
+            // }
+            // else {
+            this.setState({submitted: true})
+            // this.setState({submitted: result})
+            // }
+        }).catch((e)=> {
+
+            alert(e)
+        })
+    }
+
+    render() {
+        if(this.state.submitted) {
+            return (
+                <BrowserRouter>
+                    <Redirect push to="/main" />
+                </BrowserRouter>
+                )
+        }
+        else {
+            return (
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Username:
+                            <input type="text" value={this.state.username} onChange={this.handleUserChange}/>
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" value={this.state.password} onChange={this.handlePassChange}/>
+                        </label>
+                        <input type="submit" value="Submit"/>
+                    </form>
+            );
+        }
+    }
 }
 
 export default App;
+
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Main extends React.Component {
+
+    render() {
+        return (
+            <div>Hi dsdssdsdsdsd</div>
+        )
+    }
+
+}
+
+export default Main
